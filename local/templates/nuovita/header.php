59acf31f83d6e6c9bd07d56e8a75ce49850d0270
Admin <git@webgk.ru>
2016-06-20 15:25:33 +0300

    work copy update

--- local/templates/nuovita/header.php
+++ local/templates/nuovita/header.php
@@ -55,27 +55,27 @@
                         </div>
                         <div class = "col-xs-12 col-md-8 header-link-1">
                             <?$APPLICATION->IncludeComponent(
-                                    "bitrix:menu",
-                                    "top_menu",
-                                    array(
-                                        "ROOT_MENU_TYPE" => "top",
-                                        "MAX_LEVEL" => "2",
-                                        "CHILD_MENU_TYPE" => "left",
-                                        "USE_EXT" => "Y",
-                                        "MENU_CACHE_TYPE" => "A",
-                                        "MENU_CACHE_TIME" => "36000000",
-                                        "MENU_CACHE_USE_GROUPS" => "Y",
-                                        "MENU_CACHE_GET_VARS" => array(
-                                        ),
-                                        "COMPONENT_TEMPLATE" => "top_menu",
-                                        "DELAY" => "N",
-                                        "ALLOW_MULTI_SELECT" => "N"
-                                    ),
-                                    false,
-                                    array(
-                                        "ACTIVE_COMPONENT" => "Y"
-                                    )
-                                );?>
+	"bitrix:menu", 
+	"top_menu", 
+	array(
+		"ROOT_MENU_TYPE" => "top",
+		"MAX_LEVEL" => "2",
+		"CHILD_MENU_TYPE" => "left",
+		"USE_EXT" => "Y",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "36000000",
+		"MENU_CACHE_USE_GROUPS" => "N",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"COMPONENT_TEMPLATE" => "top_menu",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N"
+	),
+	false,
+	array(
+		"ACTIVE_COMPONENT" => "Y"
+	)
+);?>
                         </div>
 
                         <div class = "col-xs-12 col-md-2 m-search">
@@ -100,27 +100,27 @@
                         </div>
                         <div class = "col-xs-12 col-md-10 header-link-2">
                             <?$APPLICATION->IncludeComponent(
-                                    "bitrix:menu",
-                                    "top_menu",
-                                    array(
-                                        "ROOT_MENU_TYPE" => "catalogmanu",
-                                        "MAX_LEVEL" => "1",
-                                        "CHILD_MENU_TYPE" => "catalogmanu",
-                                        "USE_EXT" => "Y",
-                                        "MENU_CACHE_TYPE" => "A",
-                                        "MENU_CACHE_TIME" => "36000000",
-                                        "MENU_CACHE_USE_GROUPS" => "Y",
-                                        "MENU_CACHE_GET_VARS" => array(
-                                        ),
-                                        "COMPONENT_TEMPLATE" => ".default",
-                                        "DELAY" => "N",
-                                        "ALLOW_MULTI_SELECT" => "N"
-                                    ),
-                                    false,
-                                    array(
-                                        "ACTIVE_COMPONENT" => "Y"
-                                    )
-                                );?>
+	"bitrix:menu", 
+	"top_menu", 
+	array(
+		"ROOT_MENU_TYPE" => "catalogmanu",
+		"MAX_LEVEL" => "1",
+		"CHILD_MENU_TYPE" => "catalogmanu",
+		"USE_EXT" => "Y",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_TIME" => "36000000",
+		"MENU_CACHE_USE_GROUPS" => "N",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"COMPONENT_TEMPLATE" => "top_menu",
+		"DELAY" => "N",
+		"ALLOW_MULTI_SELECT" => "N"
+	),
+	false,
+	array(
+		"ACTIVE_COMPONENT" => "Y"
+	)
+);?>
                         </div>
                     </div>
                 </div>
@@ -129,70 +129,70 @@
             <div class = "row">
 
                 <?$APPLICATION->IncludeComponent(
-                        "bitrix:news.list",
-                        "slider_list_template",
-                        array(
-                            "DISPLAY_DATE" => "Y",
-                            "DISPLAY_NAME" => "Y",
-                            "DISPLAY_PICTURE" => "Y",
-                            "DISPLAY_PREVIEW_TEXT" => "Y",
-                            "AJAX_MODE" => "N",
-                            "IBLOCK_TYPE" => "-",
-                            "IBLOCK_ID" => "6",
-                            "NEWS_COUNT" => "1",
-                            "SORT_BY1" => "ACTIVE_FROM",
-                            "SORT_ORDER1" => "DESC",
-                            "SORT_BY2" => "SORT",
-                            "SORT_ORDER2" => "ASC",
-                            "FILTER_NAME" => "",
-                            "FIELD_CODE" => array(
-                                0 => "",
-                                1 => "",
-                            ),
-                            "PROPERTY_CODE" => array(
-                                0 => "details_link",
-                                1 => "",
-                            ),
-                            "CHECK_DATES" => "Y",
-                            "DETAIL_URL" => "",
-                            "PREVIEW_TRUNCATE_LEN" => "",
-                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
-                            "SET_TITLE" => "Y",
-                            "SET_BROWSER_TITLE" => "Y",
-                            "SET_META_KEYWORDS" => "Y",
-                            "SET_META_DESCRIPTION" => "Y",
-                            "SET_LAST_MODIFIED" => "N",
-                            "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-                            "ADD_SECTIONS_CHAIN" => "Y",
-                            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-                            "PARENT_SECTION" => "",
-                            "PARENT_SECTION_CODE" => "",
-                            "INCLUDE_SUBSECTIONS" => "Y",
-                            "CACHE_TYPE" => "A",
-                            "CACHE_TIME" => "36000000",
-                            "CACHE_NOTES" => "",
-                            "CACHE_FILTER" => "N",
-                            "CACHE_GROUPS" => "Y",
-                            "PAGER_TEMPLATE" => ".default",
-                            "DISPLAY_TOP_PAGER" => "N",
-                            "DISPLAY_BOTTOM_PAGER" => "Y",
-                            "PAGER_TITLE" => "Новости",
-                            "PAGER_SHOW_ALWAYS" => "N",
-                            "PAGER_DESC_NUMBERING" => "N",
-                            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-                            "PAGER_SHOW_ALL" => "N",
-                            "PAGER_BASE_LINK_ENABLE" => "N",
-                            "SET_STATUS_404" => "N",
-                            "SHOW_404" => "N",
-                            "MESSAGE_404" => "",
-                            "AJAX_OPTION_JUMP" => "N",
-                            "AJAX_OPTION_STYLE" => "Y",
-                            "AJAX_OPTION_HISTORY" => "N",
-                            "COMPONENT_TEMPLATE" => "slider_list_template",
-                            "AJAX_OPTION_ADDITIONAL" => ""
-                        ),
-                        false
-                    );?>
+	"bitrix:news.list", 
+	"slider_list_template", 
+	array(
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"AJAX_MODE" => "N",
+		"IBLOCK_TYPE" => "-",
+		"IBLOCK_ID" => "6",
+		"NEWS_COUNT" => "2",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_ORDER1" => "DESC",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER2" => "ASC",
+		"FILTER_NAME" => "",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"PROPERTY_CODE" => array(
+			0 => "details_link",
+			1 => "",
+		),
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"SET_TITLE" => "Y",
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_NOTES" => "",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"PAGER_TEMPLATE" => ".default",
+		"DISPLAY_TOP_PAGER" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"PAGER_TITLE" => "Новости",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"SET_STATUS_404" => "N",
+		"SHOW_404" => "N",
+		"MESSAGE_404" => "",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"AJAX_OPTION_HISTORY" => "N",
+		"COMPONENT_TEMPLATE" => "slider_list_template",
+		"AJAX_OPTION_ADDITIONAL" => ""
+	),
+	false
+);?>
 
             </div>
             <div class = "row">
--- optovikam/index.php
+++ optovikam/index.php
@@ -1,4 +1,41 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оптовикам");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><div class="row ab">
+	<div class="hidden-xs hidden-sm col-md-1 col-lg-2">
+	</div>
+	<div class="col-xs-12 col-sm-12 col-md-7 col-lg-6">
+		<div class="row">
+			<div class="col-xs-12 col-sm-offset-1 col-sm-10 col-md-12 col-md-offset-0 col-lg-12">
+				<div class="row">
+					<div class="second-header">
+	 Официальный дистрибьютор<br>
+	ООО «Амата»
+					</div>
+					<div class="usually-text">
+	 г.&nbsp;Москва, 1-й&nbsp;Институтский проезд, д.&nbsp;3,&nbsp;стр.&nbsp;10<br>
+	 Пн-Пт: 9:00 – 18:00<br>
+	 +7 495&nbsp;518 96 03, +7 499&nbsp;174 87 29<br>
+<p>
+ <a href="mailto:info@amatagroup.ru">info@amatagroup.ru</a><br>
+ <a href="http://www.amatagroup.ru" target="_blank">www.amatagroup.ru</a>
+</div>
+					<div class="usually-text">
+ <br>
+<p>
+ <b>Амата </b>– это оптовый гипермаркет детских товаров от лучших мировых и российских производителей. Мы предлагаем широкий ассортимент продукции, учитывающий все аспекты ухода за детьми: мебель и аксессуары для детской комнаты, коляски и автокресла, средства гигиены, принадлежности для кормления, игрушки и многое другое. Понимая ваше позитивное стремление обеспечить российских покупателей, а значит, и тысячи российских малышей, только лучшими товарами, мы в свою очередь гарантируем их высочайшее качество и соответствие самым строгим стандартам.
+</p>
+<p>
+	 В нашем лице вы найдете не только удобного поставщика, у которого можно закупить все и сразу, но и надежного, стабильного и перспективного партнера. Сотрудничать с нами легко, приятно и выгодно: благодаря богатому опыту, внимательному отношению и первоклассному обслуживанию большинство наших клиентов быстро переходят в разряд постоянных, укрепляя нашу безупречную деловую репутацию.
+</p>
+</div>
+				</div>
+			</div>
+			<div class="hidden-xs hidden-sm col-md-1 col-lg-1">
+			</div>
+		</div>
+	</div>
+	<div class="hidden-xs hidden-sm col-md-5 col-lg-4">
+	</div>
+</div>
+ <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
