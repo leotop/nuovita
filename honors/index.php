efe7841e97c0136bdf5b9bc2fc3982a6aa447083
Admin <git@webgk.ru>
2016-07-08 14:54:26 +0300

    Updates from production

--- honors/index.php
+++ honors/index.php
@@ -1,8 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Наши награды");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><p align="center">
+<img border="0" src="http://nuovita.ru/upload/medialibrary/2e7/2e72fcce5af3b612c43dee1362b1ba9a.jpg" width="500" height="707"></p>
+<p align="center">
+<img border="0" src="http://nuovita.ru/upload/medialibrary/b08/b082a10170bf3430434e5a564e54454d.jpg" width="500" height="707"></p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
