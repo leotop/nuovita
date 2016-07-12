90b7e68940922ca4260c3bf1acf09762b3426fbb
Sergey <st@webgk.ru>
2016-07-12 11:43:57 +0300

    SUP-4614: edit

--- local/templates/.default/js/main.js
+++ local/templates/.default/js/main.js
@@ -1,9 +1,4 @@
 function toggle_product(shop, button_span) {
     $('.' + shop).slideToggle( "slow" );
-    if ( $("." + button_span).hasClass( "top" ) ) {
-        $("." + button_span).removeClass( "top" );
-    } else {
-        $("." + button_span).addClass( "top" );
-    }
-
+    $("." + button_span).toggleClass( "top" );
 }
